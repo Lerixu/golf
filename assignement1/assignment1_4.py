a5389b48acbe0190bb076ae00d687d883c49c345
Lerixu <mtrj.adam@gmail.com>
2016-10-31 15:07:42 +0100

    Initial commit
    A first draft of the assignment, we need a latex file and revise
    question 2 and 3 maybe.

---
+++ assignement1/assignment1_4.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Introduction to Web Science
+Assignment 1
+
+Team : golf
+"""
+
+# modules
+import random
+import math
+import matplotlib.pyplot as mplot
+
+# variables
+
+#Array to store the 10 random numbers between 0 and 90
+randomSample = []
+
+#Arrays to store the values of sine and cosine on the sample
+sin = []
+cosin = []
+
+#initializing the sample
+randomSample = random.sample(range(0,90),10)
+print("The random sample used is : %s" %randomSample)
+
+#calculating the sine 
+sin = [math.sin(i) for i in randomSample]
+print("Applying sine to the sample : %s" %sin)
+
+#calculating the cosine
+cosin = [math.cos(i) for i in randomSample]
+print("Applying cosine to the sample : %s" %cosin)
+
+#ploting sin in red and cosin in yellow
+mplot.plot(randomSample, sin,'r',label="Calculated sine")
+mplot.plot(randomSample,cosin,'b',label="Calculated cosine")
+mplot.ylabel('Calculated value')
+mplot.xlabel('Sample')
+mplot.grid(True)
+
+#legend properties
+mplot.legend(bbox_to_anchor=(0,1.01,1,0),loc=3, ncol=2, mode="expand", borderaxespad=0)
+mplot.show()
+
