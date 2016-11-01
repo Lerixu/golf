b57f7b06259462b102dab36a06b93d7fc6fa7b76
Lerixu <mtrj.adam@gmail.com>
2016-11-01 21:57:50 +0100

    Commit for assignment

--- assignement1/assignment1_4.py
+++ assignement1/assignment1_4.py
@@ -20,10 +20,13 @@ randomSample = []
 sin = []
 cosin = []
 
+#1. Generate a random number sequence of 10 values between 0 to 90
 #initializing the sample
-randomSample = random.sample(range(0,90),10)
+randomSample =  random.sample(range(0,90),10)
 print("The random sample used is : %s" %randomSample)
 
+#2. Perform Sine and Cosine operation on numbers generated
+#3. Store the values in two different arrays named SIN & COSIN respectively
 #calculating the sine 
 sin = [math.sin(i) for i in randomSample]
 print("Applying sine to the sample : %s" %sin)
@@ -32,14 +35,16 @@ print("Applying sine to the sample : %s" %sin)
 cosin = [math.cos(i) for i in randomSample]
 print("Applying cosine to the sample : %s" %cosin)
 
+#4. Plot the values of SIN & COSIN in two different colors
 #ploting sin in red and cosin in yellow
 mplot.plot(randomSample, sin,'r',label="Calculated sine")
 mplot.plot(randomSample,cosin,'b',label="Calculated cosine")
+
+#5. The plot should have labeled axes and legend.
 mplot.ylabel('Calculated value')
 mplot.xlabel('Sample')
 mplot.grid(True)
 
 #legend properties
-mplot.legend(bbox_to_anchor=(0,1.01,1,0),loc=3, ncol=2, mode="expand", borderaxespad=0)
+mplot.legend(bbox_to_anchor=(0,1.01,1,0),loc=3, ncol=1, mode="expand", borderaxespad=0)
 mplot.show()
-
